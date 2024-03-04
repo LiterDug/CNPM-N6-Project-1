d8d94d18b143db06fb6a464a975eac7330ecf43b
ASUS-VIVOBOOK\ADMIN <ducmanhyl2003@gmail.com>
2024-03-05 00:19:27 +0700

    update cmt

--- signup.js
+++ signup.js
@@ -1,8 +1,9 @@
-
+// querySelector returns the first element class matches a CSS selector
 const inputUsernameRegister = document.querySelector(".input-signup-username");
 const inputPasswordRegister = document.querySelector(".input-signup-password");
 const btnRegister = document.querySelector(".signup__signInButton");
 
+// Add a click event to the element with the ID btnRegister
 btnRegister.addEventListener("click", (e) => {
   e.preventDefault();
   if (
@@ -16,7 +17,9 @@ btnRegister.addEventListener("click", (e) => {
       username: inputUsernameRegister.value,
       password: inputPasswordRegister.value,
     };
+    // Creates a user object
     let json = JSON.stringify(user);
+    // Stores the JSON string in local storage using the username as the key
     localStorage.setItem(inputUsernameRegister.value, json);
     alert("Đăng Ký Thành Công");
     window.location.href = "index.html";
